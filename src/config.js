43fb56bc17ad1c33c8acfd203d59152349fb7da2
Milan Milenkovic <milenkovicmilan10@gmail.com>
2020-08-18 18:44:01 +0200

    :fire:

--- src/config.js
+++ src/config.js
@@ -3,11 +3,11 @@ export const USER_SERVER = '/api/users';
 
 
 
-export const API_URL = 'http://api.themoviedb.org/3/';
+export const API_URL = 'https://api.themoviedb.org/3/';
 export const API_KEY = 'f71e683e5ac0ab56349d72ffe446a517';
 
 
-export const IMAGE_URL ='http://image.tmdb.org/t/p/';
+export const IMAGE_URL ='https://image.tmdb.org/t/p/';
 
 //Sizes: w300, w780, w1280, original
 export const BACKDROP_SIZE = 'w1280'
